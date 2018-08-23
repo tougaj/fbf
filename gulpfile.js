7d476b21ef13f782a944b38102e6519c5bd4162f
Alexander Tougaj <sashamolodec@gmail.com>
2018-08-23 06:22:48 +0300

    Стили переведены на sass

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,20 @@ var ts = require('gulp-typescript');
 // var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 
-var less = require('gulp-less');
+// var less = require('gulp-less');
 var cleanCSS = require('gulp-clean-css');
-var path = require('path');
+// var path = require('path');
 var csslint = require('gulp-csslint');
 // var eslint = require('gulp-eslint');
 const autoprefixer = require('gulp-autoprefixer');
+
+var sass = require('gulp-sass');
 
 function onFilesChange(event) {
 	console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 }
 
 var sTSSource = ['src/*.ts', '!src/*.d.ts'];
-// let sTSSource = 'src/*.ts';
 let tsProject = ts.createProject('src/tsconfig.json');
 
 gulp.task('ts', function () {
@@ -38,40 +39,39 @@ gulp.task('ts', function () {
 			.pipe(gulp.dest('dist')),
 		tsResult.dts.pipe(gulp.dest('dist/definition'))
 	]);
-
-	// return gulp.src(sTSSource)
-	// 	.pipe(changed('.', {extension: '.js'}))
-	// 	.pipe(tsProject())
-	// 	// .pipe(eslint())
-	// 	// .pipe(eslint.format())
-	// 	// .pipe(eslint.failAfterError())
-	// 	// .pipe(babel())
-	// 	// .js.pipe(gulp.dest('.'))
-	// 	.js.pipe(gulp.dest('.'))
-	// 	.pipe(uglify())
-	// 	// .pipe(rename('fbf.min.js'))
-	// 	// .pipe(rename(function (path) {
-	// 	//     // path.dirname += './';
-	// 	//     path.basename += '.min';
-	// 	//     // path.extname = '.js';
-	// 	// }))
-	// 	.pipe(rename({
-	// 	// dirname: 'main/text/ciao',
-	// 	// basename: 'aloha',
-	// 	// prefix: 'bonjour-',
-	// 		suffix: '.min',
-	// 	// extname: '.md',
-	// 	}))
-	// 	.pipe(gulp.dest('dist'));
 });
 
-let sLessSource = 'src/*.less';
-gulp.task('less', function () {
-	return gulp.src(sLessSource)
+// let sLessSource = 'src/*.less';
+// gulp.task('less', function () {
+// 	return gulp.src(sLessSource)
+// 		.pipe(changed('.', {extension: '.css'}))
+// 		.pipe(less({
+// 			paths: [ path.join(__dirname, 'less', 'includes') ]
+// 		}))
+// 		.pipe(autoprefixer({
+// 			browsers: ['last 2 versions'],
+// 			// cascade: false
+// 		}))		
+// 		.pipe(csslint({
+// 			lookup: false,
+// 			ids: false,
+// 			shorthand: true
+// 		}))
+// 		.pipe(csslint.formatter())
+// 		.pipe(gulp.dest('.'))
+// 		.pipe(browserSync.reload({stream: true}))
+// 		.pipe(cleanCSS())
+// 		.pipe(rename({
+// 			suffix: '.min',
+// 		}))
+// 		.pipe(gulp.dest('dist'));
+// });
+
+let sSassSource = 'src/*.sass';
+gulp.task('sass', function () {
+	return gulp.src(sSassSource)
 		.pipe(changed('.', {extension: '.css'}))
-		.pipe(less({
-			paths: [ path.join(__dirname, 'less', 'includes') ]
-		}))
+		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
 			// cascade: false
@@ -89,10 +89,9 @@ gulp.task('less', function () {
 			suffix: '.min',
 		}))
 		.pipe(gulp.dest('dist'));
-	
 });
 
-gulp.task('default', ['ts', 'less'], function(callback){
+gulp.task('default', ['ts', 'sass'], function(callback){
 	browserSync.init({
 		// server: {
 		// 	files: ['./*.css', './*.js', './*.php']
@@ -105,7 +104,7 @@ gulp.task('default', ['ts', 'less'], function(callback){
 
 	gulp.watch(sTSSource, ['ts'])
 		.on('change', onFilesChange);
-	gulp.watch(sLessSource, ['less'])
+	gulp.watch(sSassSource, ['sass'])
 		.on('change', onFilesChange);
 	gulp.watch('./index.php').on('change', browserSync.reload);
 	return callback;
